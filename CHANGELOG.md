2fa0a6f74416d88440744ef9a1e3b4bff9203f5f
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2026-08-10 16:21:53 +0200

    #gg: Finish development of version 2.4.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## 2.4.2 - 2026-08-10
 
 ### Fixed
 
--- lib/src/gg_one_do_publish_version.dart
+++ lib/src/gg_one_do_publish_version.dart
@@ -9,4 +9,4 @@
 // coverage:ignore-file
 
 /// The version of the `gg_one_do_publish` package.
-const String ggOneDoPublishVersion = '2.4.1';
+const String ggOneDoPublishVersion = '2.4.2';
